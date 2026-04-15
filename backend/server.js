145ffaa7da5b1df2fc8382e00321dccf06d691f3
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-04-15 10:44:57 -0400

    refactor: Usa função async. Espera o mongoDB conectar e só depois inicia o servidor

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,25 @@
 // Importa o app (de outro arquivo) e o cors 
+require("dotenv").config(); // Importa dotenv (globalmente) para variáveis de ambiente
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
-require("dotenv").config(); // Importa dotenv (globalmente) para variáveis de ambiente
-
 // Rota teste
 app.get("/", (req, res) => {
     res.send("Servidor está funcionando")
 })
 
-// Inicia o servidor
-app.listen(process.env.PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${process.env.PORT}`)
-})
-
-// Chama a função que conecta ao mongoDb
-connectDB()
 // Função async que inicia o servidor e conecta ao mongoDB
+const startServer = async() => {
+    try{
+        await connectDB(); // Continua o bloco apenas se "connectDB" for realizado
+        app.listen(process.env.PORT, () => {
+            console.log(`Servidor rodando em http://localhost:${process.env.PORT}`)
+        });
+    } catch(error) {
+        console.log(`Erro: ${error}`)
+    }
+}
+
+startServer();
+
+
+
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-require("dotenv").config(); // Importa dotenv para variáveis de ambiente
 const connectDB = async() => {
     try{
         await mongoose.connect(process.env.MONGO_URI);
