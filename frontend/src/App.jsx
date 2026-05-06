bf5016a6d55b00ca8d0fa090ea9f2d67c5d7540d
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-05-05 22:40:30 -0400

    feat: Add Sidebar para desktop e mobile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg';
 import viteLogo from './assets/vite.svg';
 import heroImg from './assets/hero.png';
 import './App.css';
-import SideBar from './components/SideBar/Desktop';
+import SideBar from './components/SideBar/SideBar.jsx';
 function App() {
   const [count, setCount] = useState(0)
 
---
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -0,0 +1,36 @@
+import "./sidebar.css"
+import { useState } from "react";
+export default function SideBar() {
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
+    return(
+        <>
+            <div className={menuIsOpen ? "sidebar" : "sidebar mobile-open"} id="sidebar">
+                <div className="sidebar-header">
+                    <h2><i className="fas fa-graduation-cap"></i> Gestão Alunos</h2>
+                </div>
+                <nav className="sidebar-nav">
+                    <a href="#dashboard" className="nav-item active" data-section="dashboard">
+                        <i className="fa-solid fa-table-columns"></i> Dashboard
+                    </a>
+                    <a href="#alunos" className="nav-item" data-section="alunos">
+                        <i className="fas fa-users"></i> Alunos
+                    </a>
+                    <a href="#turmas" className="nav-item" data-section="turmas">
+                        <i className="fas fa-chalkboard-teacher"></i> Turmas
+                    </a>
+                    <a href="#presenca" className="nav-item" data-section="presenca">
+                        <i className="fas fa-calendar-check"></i> Presença
+                    </a>
+                    <a href="#financeiro" className="nav-item" data-section="financeiro">
+                        <i className="fas fa-money-bill-wave"></i> Financeiro
+                    </a>
+                </nav>
+                
+                
+            </div>
+            <button onClick={() => setMenuIsOpen(!menuIsOpen)} class="mobile-menu-btn" id="mobileMenuBtn">
+                <i class="fas fa-bars"></i>
+            </button>
+        </>
+    )
+};
