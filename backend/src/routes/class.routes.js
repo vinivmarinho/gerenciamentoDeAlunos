61689de58fd057f765757bf2da23a4f44dd7984d
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-05-25 11:21:02 -0400

    fix: Corrige nome da rota

--- backend/src/routes/class.routes.js
+++ backend/src/routes/class.routes.js
@@ -5,6 +5,6 @@ const classControllers = require("../controllers/classController");
 router.post("/", classControllers.createClass);
 router.get("/", classControllers.showClasses);
 router.delete("/:id", classControllers.deleteClass);
-router.put("/:id", classcontrollers.updateClass);
+router.put("/:id", classControllers.updateClass);
 
 module.exports = router;
