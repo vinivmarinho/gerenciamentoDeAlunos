1a70cc85b37d9888e932ea3ff7ec9ea35b8b5653
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-05-28 22:19:29 -0400

    feat: Adiciona schema de pagamento no mongoose

---
+++ backend/src/models/payment.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const paymentSchema = new mongoose.Schema({
+    // Relaciona um pagamento a um aluno
+    student: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Student"
+    },
+    referenceMonth: {
+        type: String,
+        required: true
+    },
+    // Valor do pagamento
+    amount: {
+        type: Number,
+        required: true
+    },
+    dueDate: {
+        type: Date,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["Pendente", "Pago", "Atrasado"]
+    }
+})
