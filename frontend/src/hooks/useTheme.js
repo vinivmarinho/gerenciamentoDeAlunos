24097f60d8fe3a74119177859c71d07cfd77c5f3
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-06-02 11:41:31 -0400

    feat: Adiciona hook com função de alternar entre modo claro e escuro

---
+++ frontend/src/hooks/useTheme.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+
+function getInitialTheme() {
+    // Busca o tema inicial da página em localStorage
+    const stored = localStorage.getItem("theme");
+    if (stored === "light" || stored === "dark") {
+        return stored;
+    }
+    // Verifica preferência do usuário 
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        return "dark";
+    }
+    return "light";
+}
+
+export default function useTheme() {
+    const [theme, setTheme] = useState(getInitialTheme);
+
+    useEffect(() => {
+        /* Coloco o atributo na tag <html> inteira e salva no localStorage*/
+        document.documentElement.setAttribute("data-theme", theme);
+        localStorage.setItem("theme", theme);
+    }, [theme]);
+
+    function toggleTheme() {
+        setTheme(() => {
+            if (theme === "light") {
+                theme = "dark"
+            } else {
+                theme = "light"
+            }
+        });
+
+    }
+
+    return { theme, toggleTheme };
+}
