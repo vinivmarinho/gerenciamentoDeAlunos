178e71007b5a0037e8a29389dc528262ec138144
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-06-05 15:08:18 -0400

    adiciona input para procurar aluno, somente se a opção 'individual' estiver marcado

--- frontend/src/components/FinanceForm/FinanceForm.jsx
+++ frontend/src/components/FinanceForm/FinanceForm.jsx
@@ -1,14 +1,21 @@
 import "./financeForm.css";
+import { useState } from "react";
+
 export default function FinanceForm() {
-    /* O "form-modal-backdrop" e "form-modal" irão para o componente "Finance" futuramente. Eles irão cobrir o componente "FinanceForm" que será chamado por lá*/
+    
+    const [monthlyFeeType, setMonthlyFeeType] = useState("all");
+
     return(
         <form className="student-form">
             {/* Adicionar função que pesquisa os alunos disponíveis para enviar matrícula. Mostrar todos os alunos cadastrados*/}
+            <h2>Gerar mensalidades</h2>
             <input
             className="monthly-fee-radio"
             type="radio"
             id="all-students"
             name="monthly-fee-type"
+            checked={monthlyFeeType === "all"} 
+            onChange={() => setMonthlyFeeType("all")}
             >
             </input>
 
@@ -21,6 +28,8 @@ export default function FinanceForm() {
             type="radio"
             id="individual-student"
             name="monthly-fee-type"
+            checked={monthlyFeeType === "individual"}
+            onChange={() => setMonthlyFeeType("individual")}
             >
             </input>
 
@@ -28,6 +37,10 @@ export default function FinanceForm() {
                 Aluno específico
             </label>
 
+            {monthlyFeeType === "individual" && (
+                <input placeholder="Buscar aluno..."></input>
+            )}
+            
             <button type="submit">Gerar mensalidade(s)</button>
         </form>
     )
