6770d9a645aa591b303b69c2b3122bfcde43db78
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-06-24 11:45:03 -0400

    Adiciona campos no formulário

--- frontend/src/components/FinanceForm/PaymentEditForm.jsx
+++ frontend/src/components/FinanceForm/PaymentEditForm.jsx
@@ -3,11 +3,27 @@ import "../Form/form.css";
 export default function PaymentEditForm() {
     return(
         <form className="student-form">
-            <h2>Cadastrar novo aluno</h2>
+            <h2>Editar pagamento</h2>
+            <label htmlFor="paymentStatus">Status da mensalidade</label>
+            <select
+                id="paymentStatus"
+                name="paymentStatus"
+                required
+            >
+                <option value="">Pendente</option>
+                <option value="">Pago</option>
+                <option value="">Atrasado</option>
+            </select>
+            <label htmlFor="dueDate">Data de vencimento</label>
+            <input
+                id="dueDate"
+                type="number"
+                min="1"
+                max="31"
+            >
+            </input>
 
-            
-
-            <button type="submit">Cadastrar aluno</button>
+            <button type="submit">Confirmar</button>
         </form>
     )
 }
